11de58b22446def37cf5a9d65dd1c01277775b9b
nitewoo <nitewoo@gmail.com>
2014-01-09 13:08:18 +0800

    demo form

--- src/app/demo/CtrlDemoForm.js
+++ src/app/demo/CtrlDemoForm.js
@@ -1,7 +1,15 @@
 define([
     'app'
     ], function ( app ) {
-        app.registerController( 'CtrlDemoForm', [ '$scope' ], function ( $scope ) {
+        app.registerController( 'CtrlDemoForm', [ '$scope' , function ( $scope ) {
+            $scope.colors = ['red', 'blue', 'green'];
 
-        });
+            $scope.simpleForm = {
+                inputText : '',
+                inputCheckbox : []
+            };
+            $scope.alertValues = function () {
+                alert("inputText value : " + $scope.simpleForm.inputText);
+            };
+        }]);
 });
